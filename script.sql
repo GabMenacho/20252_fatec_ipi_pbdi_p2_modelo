f3d12e57765dc9e007b807047c99975bd7ed938a
Aluno <aluno@IPIRANGA.FATEC>
2025-11-25 10:10:10 -0300

    feat(p2): cria as tabelas dimensao

--- script.sql
+++ script.sql
@@ -1,1 +1,30 @@
 CREATE DATABASE p2_video_game_sales
+
+CREATE TABLE dim_tempo (
+    data_key INTEGER PRIMARY KEY,
+    ano INTEGER
+);
+ 
+CREATE TABLE dim_plataforma (
+    plataforma_key INTEGER PRIMARY KEY,
+    nome VARCHAR(50)
+);
+ 
+CREATE TABLE dim_genero (
+    genero_key INTEGER PRIMARY KEY,
+    genero VARCHAR(50)
+);
+ 
+CREATE TABLE dim_editora (
+    editora_key INTEGER PRIMARY KEY,
+    edtora VARCHAR(100)
+);
+ 
+CREATE TABLE dim_jogo (
+    jogo_key INTEGER PRIMARY KEY,
+    nome VARCHAR(255),
+    rank INTEGER
+);
+
+
+
